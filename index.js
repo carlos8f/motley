c3ec8c5ce34e412afd0a3c6b6fd641d5bd7c2470
Carlos Rodriguez <carlos@s8f.org>
2013-07-11 22:52:17 -0700

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+var dirname = require('path').dirname;
+
 var app = module.exports = new (require('events').EventEmitter);
 app.setMaxListeners(0);
 
 app.boot = function (cb) {
   require('./pkg')(function (err, p, pkg) {
     if (err) return cb(err);
-    app.root = p;
+    app.root = dirname(p);
     app.pkg = pkg;
-    require('./conf')(function (err, conf) {
+    require('./conf');
+    app.loadConf(function (err, conf) {
       if (err) return cb(err);
       app.conf = conf;
       cb();
