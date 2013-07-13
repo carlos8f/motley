9c329849b0014cfc5fd25bff18415990de632c54
Carlos Rodriguez <carlos@s8f.org>
2013-07-12 18:07:06 -0700

    conf example

--- example/server.js
+++ example/server.js
@@ -6,7 +6,7 @@ app.boot(function (err) {
   app.router.on('error', function (err, req, res) {
     res.render('error', {title: 'WTF', message: err.message});
   });
-  app.server.listen(3000, function () {
-    console.log('server running at http://localhost:3000/');
+  app.server.listen(app.conf.port, function () {
+    console.log('server running at http://localhost:' + app.conf.port + '/');
   });
 });
