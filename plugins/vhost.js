6fa6e8e6c529a918c33eb2f102833977df0065e7
Carlos Rodriguez <carlos@s8f.org>
2013-07-16 16:49:51 -0700

    vhost: fix fragmenting weights

--- plugins/vhost.js
+++ plugins/vhost.js
@@ -1,18 +1,12 @@
 var app = require('../');
 
 if (!app.vhost) {
-  require('./controller');
   require('./load');
   require('./router');
   app.vhost = function (hostPattern, dir) {
-    var vhost = app.controller();
-    app.load(dir).forEach(function (moduleExports) {
-      var handler = moduleExports.handler || moduleExports;
-      if (typeof handler === 'function') vhost.add(hostPattern, handler.weight, handler);
+    app.load(dir).forEach(function (me) {
+      var handler = me.handler || me;
+      if (typeof handler === 'function') app.router.add(hostPattern, handler.weight, handler);
     });
-    vhost.add(10000, function (req, res, next) {
-      next();
-    });
-    app.router.add(hostPattern, vhost.handler);
   };
 }
