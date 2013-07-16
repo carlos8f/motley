a66c27ff888553ddc5799f5859893ee9309525e0
Carlos Rodriguez <carlos@s8f.org>
2013-07-16 16:39:39 -0700

    fix require/load weirdness

--- plugins/load.js
+++ plugins/load.js
@@ -1,8 +1,11 @@
-var app = require('../');
+var app = require('../')
+  , resolve = require('path').resolve
 
 if (!app.load) {
   require('./glob');
   app.load = function (dir) {
-    return app.glob.sync(dir + '/**.js', {cwd: app.root}).map(module.parent && module.parent.require || require);
+    return app.glob.sync(dir + '/**.js', {cwd: app.root}).map(function (p) {
+      return require(resolve(app.root, p));
+    });
   };
 }
