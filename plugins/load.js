4a976fa35986616d80786f0b79a3569331104c9b
Carlos Rodriguez <carlos@s8f.org>
2013-07-16 15:38:48 -0700

    fix app.load() when called from app

--- plugins/load.js
+++ plugins/load.js
@@ -3,6 +3,6 @@ var app = require('../');
 if (!app.load) {
   require('./glob');
   app.load = function (dir) {
-    return app.glob.sync(dir + '/**.js', {cwd: app.root}).map(require);
+    return app.glob.sync(dir + '/**.js', {cwd: app.root}).map(module.parent && module.parent.require || require);
   };
 }
