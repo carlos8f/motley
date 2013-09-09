61003d98eeacfa01491ae69a731516476c61b096
Carlos Rodriguez <carlos@s8f.org>
2013-09-09 13:01:46 -0700

    fallback to cwd as templ root if ./views not found

--- middleware/render.js
+++ middleware/render.js
@@ -1,10 +1,6 @@
 var app = require('../')
   , existsSync = require('fs').existsSync
 
-var conf = app.conf.render || {root: app.root + '/views'};
-
-if (existsSync(conf.root)) {
-  require('../plugins/templ');
-  module.exports = app.templ(conf.root);
-  module.exports.weight = -1000;
-}
+require('../plugins/templ');
+module.exports = app.templ(app.conf.render);
+module.exports.weight = -1000;
