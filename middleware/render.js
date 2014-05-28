d3663de7599a567172ad5c0c5b6635e26ee4071f
Carlos Rodriguez <carlos@s8f.org>
2014-05-27 22:10:25 -0700

    pass options to templ

--- middleware/render.js
+++ middleware/render.js
@@ -1,6 +1,8 @@
 var templ = require('templ');
 
 module.exports = function (app) {
-  return templ(app.conf.views);
+  app.conf.render || (app.conf.render = {});
+  app.conf.render.cwd || (app.conf.render.cwd = app.dir);
+  return templ(app.conf.views, app.conf.render);
 };
 module.exports.weight = -1000;
