6b6c579f0f1643b14b699f5e885ecc320f2088fe
Carlos Rodriguez <carlos@s8f.org>
2014-05-29 00:50:15 -0700

    package cleanup and using static for favicon

--- afterware/favicon.js
+++
@@ -1,8 +0,0 @@
-var path = require('path')
-  , dish = require('dish')
-
-module.exports = function (app) {
-  return app.controller()
-    .get('/favicon.ico', dish.file(path.join(__dirname, '../assets/motley.ico')))
-    .handler;
-};
