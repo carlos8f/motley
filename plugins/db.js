0dc20b18cb7f3c83586fd16362d203e409219b09
Carlos Rodriguez <carlos@s8f.org>
2014-06-10 23:52:28 -0700

    smarter db paths

--- plugins/db.js
+++ plugins/db.js
@@ -1,8 +1,20 @@
 var level = require('level')
   , path = require('path')
+  , mkdirp = require('mkdirp')
+  , tmpDir = require('os').tmpDir()
 
 module.exports = function (app) {
-  var db = level(app.conf.db || path.join(app.dir, 'db'));
+  var dir = app.conf.db;
+  if (!dir && process.env.HOME && app.conf.id) {
+    dir = path.join(process.env.HOME, '.motley', app.conf.id);
+  }
+  if (!dir && tmpDir && app.conf.id) {
+    dir = path.join(tmpDir, '.motley', app.conf.id);
+  }
+  if (!dir) dir = path.join(app.dir, 'db');
+  mkdirp.sync(dir);
+
+  var db = level(dir);
   app.once('close', function () {
     db.close();
   });
