5ec127a4c5cfb904c1eb4de6d2b27b40c66ed2b3
Carlos Rodriguez <carlos@s8f.org>
2014-06-11 13:26:08 -0700

    remove example plugin and add full dir to example index

--- init/middleware/vars.js
+++ init/middleware/vars.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
   return function (req, res, next) {
     res.vars.title = app.conf.title;
+    res.vars.dir = app.dir;
     next();
   };
 };
--- init/plugins/example.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  // this is an example plugin
-  // the function below will be available as app.example
-  return function () {
-    // do something
-  };
-};
