7ffcf8b4c841a1e4506a2f1a77c7097804d83dbb
Carlos Rodriguez <carlos@s8f.org>
2014-06-17 15:37:38 -0700

    switch to downer for views

--- plugins/views.js
+++ plugins/views.js
@@ -1,4 +1,4 @@
-var Templ = require('templ').Templ;
+var Downer = require('downer').Downer;
 
 module.exports = function (app) {
   var fn = function () {
@@ -7,7 +7,7 @@ module.exports = function (app) {
       if (file.plugin.views) specs.push({globs: file.plugin.views, cwd: file.cwd});
     });
     if (specs.length) {
-      var t = new Templ(specs);
+      var t = new Downer(specs);
       app.once('close', function () {
         t.close();
       });
