4de1c3d8a9bf63a8caadf3bb89c167b47024993d
Carlos Rodriguez <carlos@s8f.org>
2014-06-17 17:45:54 -0700

    make vars middleware run before render

--- init-gist/vars_middleware.js
+++ init-gist/vars_middleware.js
@@ -1,7 +1,9 @@
 module.exports = function (app) {
   return function (req, res, next) {
+    res.vars || (res.vars = {});
     res.vars.title = app.conf.title;
     res.vars.dir = app.dir;
     next();
   };
 };
+module.exports.weight = -1100;
