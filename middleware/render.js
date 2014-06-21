b8304809095606313792a16772fb9e3227ea3401
Carlos Rodriguez <carlos@s8f.org>
2014-06-20 17:23:30 -0700

    prevent crash if no views/public dirs specified

--- middleware/render.js
+++ middleware/render.js
@@ -1,7 +1,8 @@
 module.exports = function (app) {
   if (app.views) {
-    return app.views().middleware(app.conf.render);
+    var views = app.views();
+    return views ? views.middleware(app.conf.render) : function (req, res, next) { next() };
   }
-  else return function (req, res, next) { next(); }
+  else return function (req, res, next) { next() };
 };
 module.exports.weight = -1000;
--- middleware/static.js
+++ middleware/static.js
@@ -1,7 +1,8 @@
 module.exports = function (app) {
   if (app.public) {
-    return app.public().middleware(app.conf.static);
+    var pub = app.public();
+    return pub ? pub.middleware(app.conf.static) : function (req, res, next) { next() };
   }
-  else return function (req, res, next) { next(); }
+  else return function (req, res, next) { next() };
 };
 module.exports.weight = -1000;
