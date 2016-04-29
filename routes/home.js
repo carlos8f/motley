4ca2ab5390365e1476c2dbc18e1fea015597594d
Carlos Rodriguez <carlos@s8f.org>
2016-04-28 22:26:29 -0700

    update codemap and tweak home output to show version number

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,8 @@ module.exports = function container (get) {
   return get('vendor.middler')()
     .get('/', function (req, res, next) {
       res.json({
-        'welcome': 'to ' + get('conf.site.title') + '!'
+        'welcome': 'to ' + get('conf.site.title') + '!',
+        'version': require('../package.json').version
       })
     })
     .handler
